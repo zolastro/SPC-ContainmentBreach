fa7ba65cfb07f441682fc9a88146f8196364cad6
David Ponce <zolastrodavid@gmail.com>
2018-04-04 22:26:41 +0200

    Players blinks every certain rate

--- Assets/Scripts/Player/BlinkService.cs
+++ Assets/Scripts/Player/BlinkService.cs
@@ -9,13 +9,25 @@ public class BlinkService : MonoBehaviour {
     [SerializeField] private GameObject eyelids;
     [SerializeField] private Slider blinkMeter;
 
-    private Animator eyelidsAnimator; 
+    private Animator eyelidsAnimator;
+
+    private float timeSinceLastBlink;
 
     void Start () {
         this.eyelidsAnimator = this.eyelids.GetComponent<Animator>();	
 	}
-	
+
+    public void Update()
+    {
+        this.timeSinceLastBlink += Time.deltaTime;
+    }
+
     public void Blink() {
+        this.timeSinceLastBlink = 0;
         this.eyelidsAnimator.SetTrigger("blink");
+    }
+
+    public float getTimeSinceLastBlink() {
+        return this.timeSinceLastBlink;
     }
 }
---
+++ Assets/Scripts/Player/PlayerBlink.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBlink : MonoBehaviour {
+
+    [SerializeField] private float blinkRate;
+
+    private BlinkService blinkService;
+
+	void Start () {
+        this.blinkService = this.GetComponent<BlinkService>();
+	}
+	
+	void Update () {
+        if (this.blinkService.getTimeSinceLastBlink() > blinkRate) {
+            this.blinkService.Blink();
+        }
+    }
+}
