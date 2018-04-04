8bf5769e07d91727c9f9b66d7fdb8fbf3983d1ff
David Ponce <zolastrodavid@gmail.com>
2018-04-04 19:47:41 +0200

    Player blinks when press Space

---
+++ Assets/Scripts/Player/BlinkService.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class BlinkService : MonoBehaviour {
+
+
+    [SerializeField] private GameObject eyelids;
+    [SerializeField] private Slider blinkMeter;
+
+    private Animator eyelidsAnimator; 
+
+    void Start () {
+        this.eyelidsAnimator = this.eyelids.GetComponent<Animator>();	
+	}
+	
+    public void Blink() {
+        this.eyelidsAnimator.SetTrigger("blink");
+    }
+}
---
+++ Assets/Scripts/Player/PlayerInputs.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputs : MonoBehaviour {
+
+    private BlinkService blinkService;
+
+    void Start () {
+        this.blinkService = this.GetComponent<BlinkService>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            this.blinkService.Blink();
+        }
+	}
+}
